6c56976b813638fc7d7af7df86e030502fd2a770
yuler <is.yuler@gmail.com>
2023-04-03 13:33:36 +0800

    feat: define confirm page

--- client/pages/product/id.ts
+++ client/pages/product/id.ts
@@ -3,4 +3,16 @@ import enchangePage from '../../enchange-page'
 
 const $app = getApp<IApp>()
 
-enchangePage({})
+enchangePage({
+  data: {
+    id: '',
+  },
+  onLoad(event: any) {
+    const {id} = event
+    this.setData({id})
+  },
+  gotoConfirm() {
+    const {id} = this.data
+    $app.$goto(`/pages/product/confirm?id=${id}`)
+  },
+})
