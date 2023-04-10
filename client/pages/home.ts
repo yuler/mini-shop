76342ab78b199d1d9b1a8c38b94f7b13597ab795
yuler <is.yuler@gmail.com>
2023-04-10 18:38:18 +0800

    test: openai api

--- client/pages/home.ts
+++ client/pages/home.ts
@@ -9,23 +9,64 @@ enchangePage({
     products: [],
   },
   async onLoad() {
-    const query = qs.stringify(
-      {
-        populate: {
-          poster: {
-            fields: ['url'],
-          },
-        },
+    const requestTask = wx.request({
+      url: 'https://openai-proxy.yuler.dev/v1/chat/completions',
+      method: 'POST',
+      header: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer xxx`,
       },
-      {
-        encode: false,
+      dataType: 'json',
+      // responseType: 'arraybuffer',
+      enableChunked: true,
+      data: {
+        model: 'gpt-3.5-turbo',
+        frequency_penalty: 0,
+        stream: true,
+        presence_penalty: 0,
+        max_tokens: 1024,
+        n: 1,
+        temperature: 0.5,
+        messages: [{role: 'user', content: '用英文打个招呼'}],
+        stop: ['<|im_end|>'],
       },
-    )
-    const {data} = await $app.$api({
-      url: `products?${query}`,
-      method: 'GET',
     })
-    this.setData({products: data})
+
+    setTimeout(() => {
+      requestTask.abort()
+    }, 2000)
+    requestTask.onChunkReceived(async (res: any) => {
+      const decoder = new TextDecoder('utf-8')
+      const values: string[] = decoder
+        .decode(res.data)
+        .split(/\n+/)
+        .filter(Boolean)
+        .map((str: string) => str.slice('data: '.length))
+      values.map(value => {
+        if (value === '[DONE]') return
+        const chunk = JSON.parse(value)
+        const content = chunk.choices[0].delta.content
+        console.log(content)
+      })
+    })
+
+    // const query = qs.stringify(
+    //   {
+    //     populate: {
+    //       poster: {
+    //         fields: ['url'],
+    //       },
+    //     },
+    //   },
+    //   {
+    //     encode: false,
+    //   },
+    // )
+    // const {data} = await $app.$api({
+    //   url: `products?${query}`,
+    //   method: 'GET',
+    // })
+    // this.setData({products: data})
   },
   gotoProduct(event: MP.Touch) {
     const {id} = event.currentTarget.dataset
